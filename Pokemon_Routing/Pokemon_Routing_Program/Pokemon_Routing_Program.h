45c7213401462b243a46f25a5bcd866e1987974c
Jinha <fkrlsp2@naver.com>
2017-03-30 08:20:27 +0900

    Project Start

---
+++ Pokemon_Routing/Pokemon_Routing_Program/Pokemon_Routing_Program.h
@@ -0,0 +1,32 @@
+
+// Pokemon_Routing_Program.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CPokemon_Routing_ProgramApp:
+// 이 클래스의 구현에 대해서는 Pokemon_Routing_Program.cpp을 참조하십시오.
+//
+
+class CPokemon_Routing_ProgramApp : public CWinApp
+{
+public:
+	CPokemon_Routing_ProgramApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPokemon_Routing_ProgramApp theApp;
